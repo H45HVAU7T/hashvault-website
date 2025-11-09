4db3a7c5232acc738ebc3e1642df7726d8edb1ea
Abhiram H <abhiramh21@gmail.com>
2025-11-09 23:57:21 +0530

    Team updates and UI enchancements

---
+++ src/pages/about/teams.ts
@@ -0,0 +1,222 @@
+// Management Team
+import adithya from "@avatars/management/adithya.avif";
+import abhishek from "@avatars/management/abhishek.png";
+import kruthika from "@avatars/management/kruthika.avif";
+
+// Logistics Team
+import nishitha from "@avatars/logistics/nishitha.avif";
+import kushal from "@avatars/logistics/kushal.png";
+import sreekar from "@avatars/logistics/sreekar.avif";
+import sudeeksha from "@avatars/logistics/sudeeksha.png";
+
+// Events Team
+import dhanvi from "@avatars/events/dhanvi.png";
+import urvee from "@avatars/events/urvee.png";
+import debarati from "@avatars/events/debarati.avif";
+import jeet from "@avatars/events/jeet.png"; 
+
+// Media Team
+import shreya from "@avatars/media/shreya.png";
+import prabhav from "@avatars/media/prabhav.avif";
+import janhavi from "@avatars/media/janhavi.png";
+import yashaswini from "@avatars/media/yashaswini.avif";
+import chethan from "@avatars/media/chethan.avif";
+
+// Tech Team
+import rahul from "@avatars/tech/rahul.avif";
+import abhay from "@avatars/tech/abhay.avif";
+import abhiram from "@avatars/tech/abhiram.avif";
+
+// Marketing Team
+import sakshi from "@avatars/marketing/sakshi.png";
+import jessica from "@avatars/marketing/jessica.avif";
+import samiksha from "@avatars/marketing/samiksha.png";
+import shraddha from "@avatars/marketing/shraddha.avif"; 
+import shipra from "@avatars/marketing/shipra.avif"; 
+
+// CTF Team
+import sagar from "@avatars/ctf/sagar.png";
+import jeffery from "@avatars/ctf/jeffery.png";
+import vachan from "@avatars/ctf/vachan.png";
+
+export const teams = [
+  {
+    name: "Management Team",
+    icon: "briefcase",
+    members: [
+      {
+        id: "01",
+        name: "Adithya Karthik M",
+        position: "President",
+        image: adithya,
+      },
+      {
+        id: "02",
+        name: "Abhishek",
+        position: "Vice President",
+        image: abhishek,
+      },
+      {
+        id: "03",
+        name: "Kruthika B A",
+        position: "Secretary",
+        image: kruthika,
+      },
+    ],
+  },
+  {
+    name: "Logistics Team",
+    icon: "list-checks",
+    members: [
+      {
+        id: "01",
+        name: "Nishita Sridhar",
+        position: "Team Lead",
+        image: nishitha,
+      },
+      {
+        id: "02",
+        name: "Kushal Maji",
+        position: "Team Associate",
+        image: kushal,
+      },
+      {
+        id: "03",
+        name: "Sreekar S Vasist",
+        position: "Team Associate",
+        image: sreekar,
+      },
+      {
+        id: "04",
+        name: "Sudeeksha H",
+        position: "Team Associate",
+        image: sudeeksha,
+      },
+    ],
+  },
+  {
+    name: "Events Team",
+    icon: "calendar-heart",
+    members: [
+      {
+        id: "01",
+        name: "Dhanvi M R",
+        position: "Team Lead",
+        image: dhanvi,
+      },
+      {
+        id: "02",
+        name: "Urvee S Bettadapur",
+        position: "Team Associate",
+        image: urvee,
+      },
+      {
+        id: "03",
+        name: "Debarati Mukherjee",
+        position: "Team Associate",
+        image: debarati,
+      },
+      {
+        id: "04",
+        name: "Jeet Bagdai",
+        position: "Team Associate",
+        image: jeet,
+      },
+    ],
+  },
+  {
+    name: "Media Team",
+    icon: "camera",
+    members: [
+      {
+        id: "01",
+        name: "Shreya Shri Pathak ",
+        position: "Team Lead",
+        image: shreya,
+      },
+      {
+        id: "02",
+        name: "Prabhav Pandey",
+        position: "Team Associate",
+        image: prabhav,
+      },
+      {
+        id: "03",
+        name: "Janhavi A Hebbar",
+        position: "Team Associate",
+        image: janhavi,
+      },
+      {
+        id: "04",
+        name: "Yashaswini M",
+        position: "Team Associate",
+        image: yashaswini,
+      },
+      {
+        id: "05",
+        name: "Chethan Kumar Y",
+        position: "Team Associate",
+        image: chethan,
+      },
+    ],
+  },
+  {
+    name: "Tech Team",
+    icon: "desktop",
+    members: [
+      { id: "01", name: "Rahul Aithal", position: "Team Lead", image: rahul },
+      { id: "02", name: "Abhay S", position: "Team Associate", image: abhay },
+      {
+        id: "03",
+        name: "Abhiram H",
+        position: "Team Associate",
+        image: abhiram,
+      },
+    ],
+  },
+  {
+    name: "Marketing Team",
+    icon: "megaphone-simple",
+    members: [
+      {
+        id: "01",
+        name: "Sakshi S Maliye",
+        position: "Team Lead",
+        image: sakshi,
+      },
+      {
+        id: "02",
+        name: "Jessica J Richard",
+        position: "Team Associate",
+        image: jessica,
+      },
+      {
+        id: "03",
+        name: "Samiksha D",
+        position: "Team Associate",
+        image: samiksha,
+      },
+      {
+        id: "04",
+        name: "Shraddha Shetty GR",
+        position: "Team Associate",
+        image: shraddha,
+      },
+      {
+        id: "05",
+        name: "Shipra Prashanth",
+        position: "Team Associate",
+        image: shipra ,
+      },
+    ],
+  },
+  {
+    name: "CTF Team",
+    icon: "flag-pennant",
+    members: [
+      { id: "01", name: "Sagar", position: "CTF Team", image: sagar },
+      { id: "02", name: "Jeffery", position: "CTF Team", image: jeffery },
+      { id: "03", name: "Vachan", position: "CTF Team", image: vachan },
+    ],
+  },
+];
